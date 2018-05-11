7ac23054a7c4e79e6b4255c8354609b1035c7400
Yukio Usuzumi <anohigisavay@gmail.com>
2018-05-12 01:26:21 +0800

    Top K Frequent Elements  & Decode String

---
+++ leetcode/LeetCode-CSharp/DecodeString/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace DecodeString
+{
+    public class Solution {
+        public string DecodeString(string s)
+        {
+            var cntStack = new Stack<int>();
+            var sStack = new Stack<StringBuilder>();
+            var num = 0;
+            var currSB = new StringBuilder();
+            foreach (var c in s)
+            {
+                switch (c)
+                {
+                    case char x when x >= '0' && x <= '9':
+                        num *= 10;
+                        num += x - '0';
+                        break;
+                    case '[':
+                        cntStack.Push(num);
+                        sStack.Push(currSB);
+                        currSB = new StringBuilder();
+                        num = 0;
+                        break;
+                    case ']':
+                        var repeat = cntStack.Pop();
+                        var sb = sStack.Pop();
+
+                        for (var i = 0; i < repeat; i++)
+                        {
+                            sb.Append(currSB);
+                        }
+                        currSB = sb;
+                        break;
+                    default:
+                        currSB.Append(c);
+                        break;
+                }
+            }
+
+            return currSB.ToString();
+        }
+    }
+    
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SDF");
+            var solution = new Solution();
+            var result = solution.DecodeString("3[a2[c]]");
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ leetcode/LeetCode-CSharp/TopKFrequentElements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TopKFrequentElements
+{
+    public class Solution
+    {
+        public IList<int> TopKFrequent(int[] nums, int k) {
+            var counter = new Dictionary<int, int>(nums.Length);
+            foreach (var num in nums)
+            {
+                var success = counter.TryAdd(num, 1);
+                if (!success)
+                {
+                    counter[num]++;
+                }
+            }
+            var sorter = new SortedSet<Tuple<int, int>>(
+                counter.Select(kvp => new Tuple<int, int>(kvp.Value, kvp.Key))
+                );
+            return sorter.Skip(counter.Count - k).Select(t => t.Item2).Reverse().ToList();
+        }
+    }
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var result = solution.TopKFrequent(new [] {1, 1, 1, 2, 2, 3}, 2);
+            Console.WriteLine("{0}", result);
+        }
+    }
+}
