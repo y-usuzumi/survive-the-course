6290f5d9304dd37e386a9f27ac56282621ebba83
Yukio Usuzumi <anohigisavay@gmail.com>
2018-05-12 00:17:03 +0800

    Jewels and stones

---
+++ leetcode/LeetCode-CSharp/JewelsAndStones/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace JewelsAndStones
+{
+    public class Solution
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            int count = 0;
+            var hs = new HashSet<char>(J);
+            foreach (var ch in S)
+            {
+                if (hs.Contains(ch))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+        
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            string j = "aA";
+            string s = "aAAbbbb";
+            Console.WriteLine("{0}", solution.NumJewelsInStones(j, s));
+        }
+    }
+}
