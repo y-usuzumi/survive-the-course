a6da2e41a6cf21de2fa9d4c1a7264d26a697e720
Yukio Usuzumi <anohigisavay@gmail.com>
2018-05-13 02:03:45 +0800

    24 Game & Couples Holding Hands

---
+++ leetcode/LeetCode-CSharp/CouplesHoldingHands/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CouplesHoldingHands
+{
+    public class Solution
+    {
+        int findMyCouple(int me)
+        {
+            if (me % 2 == 0)
+            {
+                return me + 1;
+            }
+            return me - 1;
+        }
+        public int MinSwapsCouples(int[] row)
+        {
+            var inverseRow = new int[row.Length];
+            var cnt = 0;
+            for (int i = 0; i < row.Length; i++)
+            {
+                inverseRow[row[i]] = i;
+            }
+            for (int i = 0; i < row.Length; i += 2)
+            {
+                var me = row[i];
+                var other = row[i + 1];
+                var myCouple = findMyCouple(me);
+                if (other == findMyCouple(me))
+                {
+                    continue;
+                }
+                var tmp = other;
+                row[i + 1] = myCouple;
+                var myCoupleIdx = inverseRow[myCouple];
+                row[myCoupleIdx] = other;
+                inverseRow[myCouple] = i + 1;
+                inverseRow[other] = myCoupleIdx;
+                cnt++;
+            }
+            return cnt;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var result = solution.MinSwapsCouples(new[] { 0, 2, 1, 3 });
+            Console.WriteLine("{0}", result);
+        }
+    }
+}
---
+++ leetcode/LeetCode-CSharp/TwentyFourGame/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace TwentyFourGame
+{
+    public class Solution
+    {
+        void FillRemaining(double[] target, double[] left, double[] middle, double[] right)
+        {
+            var idx = 1;
+            for (var i = 0; i < left.Length; i++, idx++)
+            {
+                target[idx] = left[i];
+            }
+            for (var i = 0; i < middle.Length; i++, idx++)
+            {
+                target[idx] = middle[i];
+            }
+            for (var i = 0; i < right.Length; i++, idx++)
+            {
+                target[idx] = right[i];
+            }
+        }
+
+        bool JudgePoint24(double[] nums)
+        {
+            if (nums.Length == 1)
+				return Math.Abs(nums[0] - 24) < 1e-6;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var left = new double[i];
+                for (var li = 0; li < i; li++)
+                {
+                    left[li] = nums[li];
+                }
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    var middle = new double[j - i - 1];
+                    for (int mi = i + 1; mi < j; mi++)
+                    {
+                        middle[mi - i - 1] = nums[mi];
+                    }
+
+                    var right = new double[nums.Length - j - 1];
+                    for (var ri = j + 1; ri < nums.Length; ri++)
+                    {
+                        right[ri - j - 1] = nums[ri];
+                    }
+
+                    var n1 = nums[i];
+                    var n2 = nums[j];
+                    var arr = new double[nums.Length - 1];
+                    FillRemaining(arr, left, middle, right);
+                    {
+                        // Add
+                        arr[0] = n1 + n2;
+                        if (JudgePoint24(arr))
+                        {
+                            return true;
+                        }
+                    }
+                    {
+                        // Subtract 1
+                        arr[0] = n1 - n2;
+                        if (JudgePoint24(arr))
+                        {
+                            return true;
+                        }
+                    }
+                    {
+                        // Subtract 2
+                        arr[0] = n2 - n1;
+                        if (JudgePoint24(arr))
+                        {
+                            return true;
+                        }
+                    }
+                    {
+                        // Multiply
+                        arr[0] = n1 * n2;
+                        if (JudgePoint24(arr))
+                        {
+                            return true;
+                        }
+                    }
+                    {
+                        // Divide 1
+                        arr[0] = n1 / n2;
+                        if (JudgePoint24(arr))
+                        {
+                            return true;
+                        }
+                    }
+                    {
+                        // Divide 2
+                        arr[0] = n2 / n1;
+                        if (JudgePoint24(arr))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+        
+        public bool JudgePoint24(int[] nums)
+        {
+            var dNums = nums.Select(n => (double) n).ToArray();
+            return JudgePoint24(dNums);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var result = solution.JudgePoint24(new[] {3, 3, 8, 8});
+            Console.WriteLine("{0}", result);
+        }
+    }
+}
