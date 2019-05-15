ca71c95b0f40bf1107afb18c9b9bd0f96b439151
Kenneth Zhao <kenneth.zhao@netis.com.cn>
2019-05-15 15:17:13 +0800

    Comments

--- leetcode/56-Merge_Intervals/merge_intervals/src/lib.rs
+++ leetcode/56-Merge_Intervals/merge_intervals/src/lib.rs
@@ -1,23 +1,38 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    pub fn merge(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        // 由于需要使用intervals中第一个区间作为基准，因此对空做特殊判断。
         if intervals.len() == 0 {
             return Vec::new();
         }
-        let mut sorted_intervals = intervals.clone();
-        sorted_intervals.sort_by(|a, b| a[0].cmp(&b[0]));
+
+        // 以左值为基准进行排序。
+        intervals.sort_by(|a, b| a[0].cmp(&b[0]));
+
+        // 结果集
         let mut result = Vec::new();
-        let mut curr_intv = sorted_intervals[0].clone();
-        for intv in sorted_intervals.iter().skip(1) {
-            if intv[0] > curr_intv[1] {
+
+        // 取出第0个区间。
+        let mut curr_intv = intervals[0].clone();
+
+        // 遍历剩下的区间
+        for intv in intervals.iter().skip(1) {
+            if intv[0] > curr_intv[1] {  // 不重合，
+                // 则把之前合并的结果存入结果集，
                 result.push(curr_intv);
+                // 然后以当前区间为基准继续。
                 curr_intv = intv.clone();
             } else {
+                // 如果重合，如：[1, 4] 和 [2, 5]，
+                // 则取 (4) 和 (5) 中更大的作为合并结果的右值：
+                // [1, 5]。
                 curr_intv[1] = curr_intv[1].max(intv[1]);
             }
         }
+        // 最后一个结果还没有存入结果集。
         result.push(curr_intv);
+        // 返回最终结果
         result
     }
 }
@@ -25,6 +40,7 @@ impl Solution {
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn it_works_for_sample_test_1() {
         assert_eq!(
