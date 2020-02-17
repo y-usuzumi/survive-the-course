2320f3109080675d49dc834395f36b9dfbf86738
Yukio Usuzumi <anohigisavay@gmail.com>
2020-02-17 13:32:51 +0800

    1114. Print in Order

---
+++ leetcode/1114.Print_in_Order/main.py
@@ -0,0 +1,26 @@
+from threading import Semaphore
+
+class Foo:
+    def __init__(self):
+        self._curr = 0
+        self._sem_2 = Semaphore(0)
+        self._sem_3 = Semaphore(0)
+
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self._sem_2.release()
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        self._sem_2.acquire()
+        # printSecond() outputs "second". Do not change or remove this line.
+        printSecond()
+        self._sem_3.release()
+
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        self._sem_3.acquire()
+        # printThird() outputs "third". Do not change or remove this line.
+        printThird()
