4a7ae27e22378d5c141b2283c8efb01e75683466
Yukio Usuzumi <anohigisavay@gmail.com>
2022-10-28 20:29:49 -0700

    Generic segment tree

--- survive-the-course-ts/lib/segment_tree.test.ts
+++ survive-the-course-ts/lib/segment_tree.test.ts
@@ -1,27 +1,35 @@
 import { describe, test, expect } from '@jest/globals';
-import SegmentTree from './segment_tree';
+import { MinSegmentTree, SumSegmentTree } from './segment_tree';
 
-describe('Segment tree', () => {
+describe('Sum segment tree', () => {
     test('Construct correct underlying arr', () => {
         const arr = [1, 3, 5, 7, 9, 11];
-        const st = new SegmentTree(arr);
+        const st = new SumSegmentTree(arr);
         expect(st.underlyingArray).toStrictEqual([36, 9, 27, 4, 5, 16, 11, 1, 3, 0, 0, 7, 9, 0, 0]);
     });
 
     test('Sum 2-4', () => {
         const arr = [1, 3, 5, 7, 9, 11];
-        const st = new SegmentTree(arr);
-        expect(st.sum(2, 4)).toBe(21);
+        const st = new SumSegmentTree(arr);
+        expect(st.aggregate(2, 4)).toBe(21);
     });
 
     test('Update 2, 4', () => {
         const arr = [1, 3, 5, 7, 9, 11];
-        const st = new SegmentTree(arr);
+        const st = new SumSegmentTree(arr);
         st.update(2, 8);
         st.update(4, 13);
-        expect(st.sum(2, 2)).toBe(8);
-        expect(st.sum(4, 4)).toBe(13);
-        expect(st.sum(0, 2)).toBe(12);
-        expect(st.sum(2, 5)).toBe(39);
+        expect(st.aggregate(2, 2)).toBe(8);
+        expect(st.aggregate(4, 4)).toBe(13);
+        expect(st.aggregate(0, 2)).toBe(12);
+        expect(st.aggregate(2, 5)).toBe(39);
     });
+});
+
+describe('Min segment tree', () => {
+    test('Min of 1-5', () => {
+        const arr = [2, 5, 1, 4, 9, 3];
+        const st = new MinSegmentTree(arr);
+        expect(st.aggregate(1, 5)).toBe(1);
+    })
 });
--- survive-the-course-ts/lib/segment_tree.ts
+++ survive-the-course-ts/lib/segment_tree.ts
@@ -1,74 +1,94 @@
-export default class SegmentTree {
-    private arr: number[];
+export class SegmentTree<T, U> {
+    private arr: U[];
     private length: number;
+    private singletonFunc: (v: T) => U;
+    private defaultValueFunc: () => U;
+    private aggrFunc: (left: U, right: U) => U;
 
-    constructor(arr: number[]) {
+    constructor(arr: T[], singletonFunc: (v: T) => U, defaultValueFunc: () => U, aggrFunc: (left: U, right: U) => U) {
         const leafLength = Math.pow(2, Math.ceil(Math.log2(arr.length)));
         const totalLength = 2 * leafLength - 1;
         this.arr = new Array(totalLength).fill(0);
         this.length = arr.length;
+        this.singletonFunc = singletonFunc;
+        this.defaultValueFunc = defaultValueFunc;
+        this.aggrFunc = aggrFunc;
         this.populateSegmentTree(arr, 0, 0, arr.length - 1);
     }
 
-    private populateSegmentTree(arr: number[], currIdx: number, left: number, right: number): number {
+    private populateSegmentTree(arr: T[], currIdx: number, left: number, right: number): U {
         if (left === right) {
-            this.arr[currIdx] = arr[left];
-            return arr[left];
+            const singletonResult = this.singletonFunc(arr[left]);
+            this.arr[currIdx] = singletonResult;
+            return singletonResult;
         }
         let mid = this.getMid(left, right);
-        let leftSum = this.populateSegmentTree(arr, currIdx * 2 + 1, left, mid);
-        let rightSum = this.populateSegmentTree(arr, currIdx * 2 + 2, mid+1, right);
-        let total = leftSum + rightSum;
-        this.arr[currIdx] = total;
-        return total;
+        let leftResult = this.populateSegmentTree(arr, currIdx * 2 + 1, left, mid);
+        let rightResult = this.populateSegmentTree(arr, currIdx * 2 + 2, mid+1, right);
+        let aggrResult = this.aggrFunc(leftResult, rightResult);
+        this.arr[currIdx] = aggrResult;
+        return aggrResult;
     }
 
     private getMid(left: number, right: number) {
         return Math.floor((left + right) / 2);
     }
 
-    private getSum(currIdx: number, currLeft: number, currRight: number, fromIdx: number, toIdx: number) {
+    private aggregateHelper(currIdx: number, currLeft: number, currRight: number, fromIdx: number, toIdx: number): U {
         if (currLeft > toIdx || currRight < fromIdx) {
-            return 0;
+            return this.defaultValueFunc();
         }
         if (currLeft >= fromIdx && currRight <= toIdx) {
             return this.arr[currIdx];
         }
         const mid = this.getMid(currLeft, currRight);
-        const leftSum =  this.getSum(currIdx * 2 + 1, currLeft, mid, fromIdx, toIdx);
-        const rightSum = this.getSum(currIdx * 2 + 2, mid+1, currRight, fromIdx, toIdx);
-        return leftSum + rightSum;
+        const leftResult =  this.aggregateHelper(currIdx * 2 + 1, currLeft, mid, fromIdx, toIdx);
+        const rightResult = this.aggregateHelper(currIdx * 2 + 2, mid+1, currRight, fromIdx, toIdx);
+        return this.aggrFunc(leftResult, rightResult);
     }
 
-    private updateValue(currIdx: number, currLeft: number, currRight: number, idx: number, val: number): number {
+    private updateValue(currIdx: number, currLeft: number, currRight: number, idx: number, val: T): U {
         if (currLeft > idx || currRight < idx) {
             return this.arr[currIdx];
         }
         if (currLeft == currRight) {
-            this.arr[currIdx] = val;
-            return val;
+            const singletonResult = this.singletonFunc(val)
+            this.arr[currIdx] = singletonResult;
+            return singletonResult;
         }
         const mid = this.getMid(currLeft, currRight);
         const leftResult = this.updateValue(currIdx * 2 + 1, currLeft, mid, idx, val);
         const rightResult = this.updateValue(currIdx * 2 + 2, mid+1, currRight, idx, val);
-        const newSum = leftResult + rightResult;
-        this.arr[currIdx] = newSum;
-        return newSum;
+        const aggrResult = this.aggrFunc(leftResult, rightResult);
+        this.arr[currIdx] = aggrResult;
+        return aggrResult;
     }
 
     get size(): number {
         return this.length;
     }
 
-    get underlyingArray(): number[] {
+    get underlyingArray(): U[] {
         return this.arr;
     }
 
-    sum(fromIndex: number, toIndex: number): number {
-        return this.getSum(0, 0, this.length - 1, fromIndex, toIndex);
+    aggregate(fromIndex: number, toIndex: number): U {
+        return this.aggregateHelper(0, 0, this.length - 1, fromIndex, toIndex);
     }
 
-    update(index: number, value: number) {
+    update(index: number, value: T) {
         this.updateValue(0, 0, this.length - 1, index, value);
+    }
+}
+
+export class SumSegmentTree extends SegmentTree<number, number> {
+    constructor(arr: number[]) {
+        super(arr, a => a, () => 0, (left, right) => left + right);
+    }
+}
+
+export class MinSegmentTree extends SegmentTree<number, number> {
+    constructor(arr: number[]) {
+        super(arr, a => a, () => Infinity, (left, right) => Math.min(left, right));
     }
 }
