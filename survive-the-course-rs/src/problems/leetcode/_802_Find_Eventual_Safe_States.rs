56343099b907c08514c370538e2fcc9352e1f530
Yukio Usuzumi <anohigisavay@gmail.com>
2023-01-07 16:36:33 -0800

    Leetcode 802: Find Eventual Safe States

---
+++ survive-the-course-rs/src/problems/leetcode/_802_Find_Eventual_Safe_States.rs
@@ -0,0 +1,59 @@
+// https://leetcode.com/problems/find-eventual-safe-states/
+
+use std::collections::{HashMap, VecDeque};
+
+pub struct Solution1;
+// This problem can also be solved by Top-down DP with memoization
+pub struct Solution2;
+
+impl Solution1 {
+    // Reversed topological sort
+    pub fn eventual_safe_nodes(graph: Vec<Vec<i32>>) -> Vec<i32> {
+        let len = graph.len();
+        let mut hm = HashMap::new();
+        for node in 0..len as i32 {
+            hm.insert(node, vec![]);
+        }
+
+        let mut prereqs = vec![0; len];
+
+        for (idx, children) in graph.iter().enumerate() {
+            for child in children {
+                hm.get_mut(child).unwrap().push(idx as i32);
+                prereqs[idx] += 1;
+            }
+        }
+
+        let mut q = VecDeque::new();
+        for (idx, prereq) in prereqs.iter().enumerate() {
+            if *prereq == 0 {
+                q.push_back(idx as i32);
+            }
+        }
+
+        let mut result = vec![];
+
+        while let Some(node) = q.pop_front() {
+            result.push(node);
+            for &child in hm.get(&node).unwrap() {
+                prereqs[child as usize] -= 1;
+                if prereqs[child as usize] == 0 {
+                    q.push_back(child);
+                }
+            }
+        }
+
+        result.sort();
+        return result;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        todo!();
+    }
+}
